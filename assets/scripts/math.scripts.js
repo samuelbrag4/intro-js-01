ebb104b42743c214b0f4a7a6f103ff08d948efca
Samuel Braga <samuel.tds2.senai@gmail.com>
2025-02-04 09:44:27 -0300

    feat: renames calculate function to sum and adds subtraction, multiplication and division functions

--- assets/scripts/math.scripts.js
+++ assets/scripts/math.scripts.js
@@ -1,4 +1,4 @@
-function calcular() {
+function soma() {
     let a = parseInt(document.getElementById('numA').value);
     let b = parseInt(document.getElementById('numB').value);
 
@@ -9,6 +9,39 @@ function calcular() {
     // console.log(calcular);
 
     document.getElementById('result').innerHTML = a + b;
+
+    console.log(document.getElementById('result').innerText)
+}
+
+function sub() {
+    let a = parseInt(document.getElementById('numA').value);
+    let b = parseInt(document.getElementById('numB').value);
+
+    console.log(`Valor A: ${a} | Valor B: ${b}`);
+
+    document.getElementById('result').innerHTML = a - b;
+
+    console.log(document.getElementById('result').innerText)
+}
+
+function mult() {
+    let a = parseInt(document.getElementById('numA').value);
+    let b = parseInt(document.getElementById('numB').value);
+
+    console.log(`Valor A: ${a} | Valor B: ${b}`);
+
+    document.getElementById('result').innerHTML = a * b;
+
+    console.log(document.getElementById('result').innerText)
+}
+
+function divi() {
+    let a = parseInt(document.getElementById('numA').value);
+    let b = parseInt(document.getElementById('numB').value);
+
+    console.log(`Valor A: ${a} | Valor B: ${b}`);
+
+    document.getElementById('result').innerHTML = a / b;
 
     console.log(document.getElementById('result').innerText)
 }
