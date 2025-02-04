c034336bb66f085fb37f0baa1caf6c33a74652fb
Samuel Braga <samuel.tds2.senai@gmail.com>
2025-02-04 08:42:18 -0300

    feat: updates displayName function to display message on page

--- assets/scripts/index.scripts.js
+++ assets/scripts/index.scripts.js
@@ -1,9 +1,17 @@
-function exibirNome () {
+function exibirNome() {
     let nome = document.getElementById('nome').value; // o .value mostra que a função deve pegar o valor que existe dentro do elemento html com id "nome"
 
     console.log(nome);
 
-    alert(`Olá ${nome}!`);
+    let mensagem = document.getElementById('msg').innerHTML = `Olá ${nome}!`
+
+    /*
+    .innerHTML - pode ser usado para alterar/criar elementos de forma mais abrangente. Podendo até mesmo criar tags
+
+    .innerText - usado para mudar somente o texto contido em um elemento
+    */
+
+    console.log(mensagem)
 }
 
 exibirNome();
